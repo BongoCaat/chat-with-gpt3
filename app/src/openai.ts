0a39f274c0efda7f58d8b38796d7bb21b6b19087
bongocaat <valentinopietropaolo70@gmail.com>
2023-03-16 19:09:16 -0300

    modified:   app/src/openai.ts

--- app/src/openai.ts
+++ app/src/openai.ts
@@ -5,7 +5,6 @@ import { OpenAIMessage, Parameters } from "./types";
 
 export const defaultSystemPrompt = `
 You are ChatGPT, a large language model trained by OpenAI.
-Knowledge cutoff: 2021-09
 Current date and time: {{ datetime }}
 `.trim();
 
